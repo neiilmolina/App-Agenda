8ae4ca700ea5154f6d77fa1a993c4cea76d94bbd
Dennis-Hidalgo <dennisf00@hotmail.com>
2024-03-07 09:12:30 +0100

    vista login

--- app/src/main/java/com/example/appagenda/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/example/appagenda/ui/settings/SettingsActivity.kt
@@ -1,44 +1,74 @@
 package com.example.appagenda.ui.settings
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.appagenda.R
+import com.example.appagenda.databinding.FragmentLoginBinding
+import com.example.appagenda.databinding.FragmentSettingsBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
-class SettingsActivity : Activity() {
+import com.google.firebase.auth.auth
+import com.google.firebase.Firebase
+class SettingsActivity : AppCompatActivity() {
     // [START declare_auth]
     private lateinit var auth: FirebaseAuth
+    private var _binding: FragmentLoginBinding? = null
+    private val binding: FragmentLoginBinding
+        get() = _binding!!
+
     // [END declare_auth]
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.fragment_login)
+        _binding = FragmentLoginBinding.inflate(layoutInflater)
         // [START initialize_auth]
         // Initialize Firebase Auth
         auth = Firebase.auth
+        init()
         // [END initialize_auth]
+
     }
+
     // [START on_start_check_user]
     public override fun onStart() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
         if (currentUser != null) {
-            reload()
+            updateUI(currentUser)
         }
     }
     // [END on_start_check_user]
+
+    private fun init(){
+        val correo = "dasdas@yopmail.com"
+        val pass = "1234567"
+        Log.d(TAG, "CORREOOO" + correo)
+        Log.d(TAG, "PASSSS" + pass)
+        binding.btnLogin.setOnClickListener { createAccount(correo,pass) }
+
+
+        Log.d(TAG, "INICIADO")
+    }
     private fun createAccount(email: String, password: String) {
+        Log.d(TAG, "CREADAAA")
         // [START create_user_with_email]
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "createUserWithEmail:success")
+
                     val user = auth.currentUser
                     updateUI(user)
                 } else {
                     // If sign in fails, display a message to the user.
+
                     Log.w(TAG, "createUserWithEmail:failure", task.exception)
                     Toast.makeText(
                         baseContext,
@@ -74,9 +104,10 @@ class SettingsActivity : Activity() {
     }
     private fun updateUI(user: FirebaseUser?) {
     }
+
     private fun reload() {
     }
     companion object {
-        private const val TAG = "EmailPassword"
+        const val TAG = "EmailPassword"
     }
 }
--- app/src/main/java/com/example/appagenda/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/appagenda/ui/settings/SettingsFragment.kt
@@ -1,15 +1,21 @@
 package com.example.appagenda.ui.settings
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.appagenda.MainActivity
+import com.example.appagenda.R
 import com.example.appagenda.databinding.FragmentSettingsBinding
+import com.example.appagenda.ui.DetallesTarea.DetallesTareaActivity
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.auth
 
 class SettingsFragment : Fragment() {
@@ -18,25 +24,26 @@ class SettingsFragment : Fragment() {
 
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val binding get() = _binding!!
+    private val binding:FragmentSettingsBinding get() = _binding!!
 
-    override fun onCreateView(
 
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        auth = Firebase.auth
-        val settingsViewModel = ViewModelProvider(this).get(SettingsViewModel::class.java)
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-
-
-        return root
+        return binding.root
     }
 
 
+
+
+    private fun init() {
+        binding.btnLogin.setOnClickListener { logear() }
+    }
+
+    private fun logear() {
+        val intent = Intent(requireContext(), SettingsActivity::class.java)
+        startActivity(intent)
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
