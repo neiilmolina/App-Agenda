d5761040563d404b683ea05e2e4986225e641c2e
Neil Molina <neilmolinarellano04@gmail.com>
2024-03-14 23:12:03 +0100

    Editar y SplashActivity

--- app/src/main/java/com/example/appagenda/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/appagenda/ui/home/HomeFragment.kt
@@ -28,8 +28,6 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this)[HomeViewModel::class.java]
         auth = Firebase.auth
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
